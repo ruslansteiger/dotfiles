d2d69988669437b14c284b7b45afee26cf57a322
Ruslan Steiger <steiger.ruslan@gmail.com>
2021-12-26 20:59:25 +0100

    Added ls aliases

--- aliases.zsh
+++ aliases.zsh
@@ -3,3 +3,9 @@
 alias nah='git reset --hard;git clean -df'
 # wip
 alias wip='git add . && git commit -m wip'
+
+### Make ls great again
+# colorize the output
+alias ls='ls --color'
+# show hidden files + long listing format + file size in MB
+alias ll='ls -alh'
