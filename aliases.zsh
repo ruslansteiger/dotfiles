8e406d71e0e4cce5848f22df3ca93864b60a16e5
Ruslan Steiger <steiger.ruslan@gmail.com>
2021-12-30 14:40:06 +0100

    Added laravel aliases

--- aliases.zsh
+++ aliases.zsh
@@ -4,6 +4,14 @@ alias nah='git reset --hard;git clean -df'
 # wip
 alias wip='git add . && git commit -m wip'
 
+### Laravel
+alias a='php artisan'
+alias mfs='php artisan migrate:fresh --seed'
+alias p='php artisan test'
+function pf() {
+  php artisan test --filter "$*"
+}
+
 ### Make ls great again
 # colorize the output
 alias ls='ls --color'
