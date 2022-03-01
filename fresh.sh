97659bd4fb7502578899ea0e5cbc74af01cd6342
Ruslan Steiger <steiger.ruslan@gmail.com>
2022-03-01 16:36:06 +0100

    Activate rebase on pull

--- fresh.sh
+++ fresh.sh
@@ -51,8 +51,8 @@ git config --global user.name "Ruslan Steiger"
 # Set vim as default editor
 git config --global core.editor "vim"
 
-# Deactivate rebase on pull
-git config --global pull.rebase false
+# Activate rebase on pull
+git config --global pull.rebase true
 
 # Add global gitignore
 git config --global core.excludesfile ~/.dotfiles/.gitignore_global
